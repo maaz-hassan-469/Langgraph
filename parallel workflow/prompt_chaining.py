f58c497c5a2056c2bad4cc864712bed24c8c79c8
maaz-hassan-469 <maazhassaan469@gmail.com>
2026-09-09 09:28:57 -0700

    add graph edges

--- parallel workflow/prompt_chaining.py
+++ parallel workflow/prompt_chaining.py
@@ -54,4 +54,18 @@ graph.add_node("evaluate_analysis",evaluate_analysis)
 graph.add_node("evaluate_thought",evaluate_thought)
 graph.add_node("final_evaluation",final_evaluation)
 
+graph.add_edge(START,"evaluate_language")
+graph.add_edge(START,"evaluate_analysis")
+graph.add_edge(START,"evaluate_thought")
 
+graph.add_edge("evaluate_language","final_evaluation")
+graph.add_edge("evaluate_analysis","final_evaluation")
+graph.add_edge("evaluate_thought","final_evaluation")
+
+graph.add_edge("final_evaluation",END)
+
+workflow=graph.compile()
+
+initial_state={"essay":essay}
+result=workflow.invoke(initial_state)
+print(result)
