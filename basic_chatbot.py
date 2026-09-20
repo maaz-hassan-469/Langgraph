fd6b35fe9fd7de8779eccc4d8d82daf83b1d5d5d
maaz-hassan-469 <maazhassaan469@gmail.com>
2026-09-20 03:05:04 -0700

    add temporary memory in this chatbot

--- basic_chatbot.py
+++ basic_chatbot.py
@@ -1,14 +1,16 @@
 from langgraph.graph import StateGraph,START,END
 from typing import TypedDict,Annotated,List
 from langchain_ollama import ChatOllama
-from langchain_core.messages import BaseMessage,HumanMessage,SystemMessage
+from langchain_core.messages import BaseMessage,HumanMessage
 from langgraph.graph.message import add_messages
+from langgraph.checkpoint.memory import MemorySaver
 
 model=ChatOllama(model="llama3.2:1b")
 
 class llmstate(TypedDict):
     messages:Annotated[List[BaseMessage],add_messages]
 
+checkpointer=MemorySaver()
 graph=StateGraph(llmstate)
 
 def chatbot(state:llmstate):
@@ -22,14 +24,15 @@ graph.add_node("chatbot",chatbot)
 graph.add_edge(START,"chatbot")
 graph.add_edge("chatbot",END)
 
-workflow=graph.compile()
+workflow=graph.compile(checkpointer=checkpointer)
 
+thread_id="1"
 while True:
     user_message=input("type here:")
     if user_message.strip().lower() in ["exit","bye","quit"]:
         break
-
-    response=workflow.invoke({"messages":[HumanMessage(content=user_message)]})
+    config={"configurable":{"thread_id":thread_id}}
+    response=workflow.invoke({"messages":[HumanMessage(content=user_message)]},config=config)
     print("AI:",response["messages"][-1].content)
 
 
